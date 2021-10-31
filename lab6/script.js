c6eefbe457b1aa51cb7dc5bc9cd84528eb59d238
RzaevNR <rzaevnr@list.ru>
2021-10-31 14:57:34 +0300

    feat: add lab 6

---
+++ lab6/script.js
@@ -0,0 +1,9 @@
+const start = Date.now()
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log(`DOM fully loaded and parsed; Time: ${Date.now() - start} ms`)
+})
+
+window.addEventListener('load', () => {
+  console.log(`Page is fully loaded; Time: ${Date.now() - start} ms`)
+})
